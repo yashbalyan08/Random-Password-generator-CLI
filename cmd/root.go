7fdedf40a3a84fe553e1583692fb0b2692b20ae0
yashbalyan08 <yashbalyan08@gmail.com>
2024-09-19 14:04:07 +0530

    final commit

--- cmd/root.go
+++ cmd/root.go
@@ -22,6 +22,7 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
